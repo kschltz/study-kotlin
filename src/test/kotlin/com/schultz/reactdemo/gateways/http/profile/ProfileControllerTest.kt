77ced0164409a17aaf2783d19e0a0abc69e78bfb
kaue schultz <kaue.schultz@paygo.com.br>
2019-04-18 17:51:36 -0300

    add: test

---
+++ src/test/kotlin/com/schultz/reactdemo/gateways/http/profile/ProfileControllerTest.kt
@@ -0,0 +1,24 @@
+package com.schultz.reactdemo.gateways.http.profile
+
+import com.schultz.reactdemo.domain.Profile
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import reactor.test.StepVerifier
+
+
+@SpringBootTest
+internal class ProfileControllerTest(@Autowired private val controller: ProfileController) {
+
+    @Test
+    fun loadContext() {
+
+        controller.saveMultiProfiles(100, Profile("Kaue"))
+        val flux = controller.listAll().body ?: throw RuntimeException("flux expected")
+
+        StepVerifier.create(flux)
+                .thenConsumeWhile { it.name == "Kaue" }
+                .verifyComplete()
+
+    }
+}
